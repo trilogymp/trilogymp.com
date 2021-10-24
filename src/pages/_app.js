4cec4d9ff1511202630a556db326985d3b6d3334
cleopatra <enesakilliok17@gmail.com>
2021-10-24 19:42:37 +0300

    Add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import Head from 'next/head';
 import '@styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
